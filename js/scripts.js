6ff03703f3c2502b38de1f8d20d3d91e8d9a28df
Chris Soueidan <chris@loogart.com>
2020-11-02 12:42:29 -0500

    setup basic bones layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-
+/* hide dismissable banner */
+$(document).ready(function(){
+  $('.close-this').click(function(){
+    $('.hero-dismissable').hide();
+  });
+    
+$('.open-mobile-filters').click(function(){
+    $('.filters').addClass('d-block')
+});
+    
+$('.close-mobile-filters').click(function(){
+    $('.filters').removeClass('d-block')
+});
+  
+    
+});
