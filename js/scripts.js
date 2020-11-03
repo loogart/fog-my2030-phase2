11952776359f150fc6828b0c40086a1bed529bef
Chris Soueidan <chris@loogart.com>
2020-11-03 15:07:43 -0500

    Additional udpates
    - filter sidebar
    - cards design
    - footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,358 @@
 /* hide dismissable banner */
-$(document).ready(function(){
-  $('.close-this').click(function(){
-    $('.hero-dismissable').hide();
-  });
+$(document).ready(function () {
+    $('.close-this').click(function () {
+        $('.hero-dismissable').hide();
+    });
+
+    $('.open-mobile-filters').click(function () {
+        $('.filters').addClass('d-block');
+    });
+
+    $('.close-mobile-filters').click(function () {
+        $('.filters').removeClass('d-block');
+    });
+
+    //toggle map view
+    $(function () {
+        $("#customSwitch1").change(function () {
+            $("#resultsMap").toggleClass("d-block", this.checked);
+            $("#resultsGrid").toggleClass("d-none", this.checked);
+        }).change();
+    });
     
-$('.open-mobile-filters').click(function(){
-    $('.filters').addClass('d-block')
+    //rotate plus icons
+    $('.filter-accordion').click(function () {
+        $(this).children('.filter-plus').toggleClass('filter-plus-open');
+    });
 });
-    
-$('.close-mobile-filters').click(function(){
-    $('.filters').removeClass('d-block')
-});
-  
-    
-});
+
+// Google Maps
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: new google.maps.LatLng(55.1304, -96.3468),
+        //map customization styles
+        styles: 
+        [
+            {
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#f5f5f5"
+                  }
+                ]
+              },
+            {
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                  }
+                ]
+              },
+            {
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#616161"
+                  }
+                ]
+              },
+            {
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#f5f5f5"
+                  }
+                ]
+              },
+            {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#bdbdbd"
+                  }
+                ]
+              },
+            {   
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#eeeeee"
+                  }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                  }
+                ]
+              },
+            {   
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#e5e5e5"
+                  }
+                ]
+              },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                  }
+                ]
+              },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                      }
+                    ]
+                  },
+                    {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                      }
+                    ]
+                  },
+                {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#dadada"
+                      }
+                    ]
+                  },
+                {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#616161"
+                      }
+                    ]
+                  },
+                {   
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                      }
+                    ]
+                  },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#e5e5e5"
+                      }
+                    ]
+                  },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#eeeeee"
+                      }
+                    ]
+                  },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#c9c9c9"
+                      }
+                    ]
+                  },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                  }
+                ]
+              }
+            ]
+    });
+    // custom Le Burger Week icon for Maps
+    //    var image = './img/poutine-marker.svg';
+
+    var image = {
+        url: "./img/green-dot.svg", // url
+        scaledSize: new google.maps.Size(20, 20), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    };
+
+    // Example of my location
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 45.5035,
+            lng: -73.5695
+        },
+        map: map,
+        title: 'My location'
+    });
+
+    // First project
+    var projectMarker1 = new google.maps.Marker({
+        position: {
+            lat: 45.5035,
+            lng: -73.5685
+        },
+        map: map,
+        icon: image,
+        title: 'Project 1'
+    });
+
+    var contentString1 =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<a href="./project.html" class="grid-burger__item for-map">' +
+        '<img src="./img/photo-burger1.jpg" class="img-map" alt="...">' +
+        '<span class="resto-name">Restaurant Name</span>' +
+        '<span class="burger-name">Poutine Name</span>' +
+        '<div class="d-flex justify-content-between">' +
+        '<span class="price">$18</span>' +
+        '<span class="distance">1.2km</span>' +
+        '</div>' +
+        '</a>' +
+        '</div>';
+
+    var infowindow1 = new google.maps.InfoWindow({
+        content: contentString1
+    });
+    projectMarker1.addListener('click', function () {
+        infowindow1.open(map, projectMarker1);
+        infowindow2.close(map, projectMarker2);
+        infowindow3.close(map, projectMarker3);
+        infowindow4.close(map, projectMarker4);
+    });
+
+    // Second project
+    var projectMarker2 = new google.maps.Marker({
+        position: {
+            lat: 53.1355,
+            lng: -57.6604
+        },
+        map: map,
+        icon: image,
+        title: 'Project 2'
+    });
+    var contentString2 =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<a href="./burger.html" class="grid-burger__item for-map">' +
+        '<img src="./img/photo-burger2.jpg" class="img-map" alt="...">' +
+        '<span class="resto-name">Restaurant Name</span>' +
+        '<span class="burger-name">Poutine Name</span>' +
+        '<div class="d-flex justify-content-between">' +
+        '<span class="price">$18</span>' +
+        '<span class="distance">1.2km</span>' +
+        '</div>' +
+        '</a>' +
+        '</div>';
+
+    var infowindow2 = new google.maps.InfoWindow({
+        content: contentString2
+    });
+    projectMarker2.addListener('click', function () {
+        infowindow2.open(map, projectMarker2);
+        infowindow1.close(map, projectMarker1);
+        infowindow3.close(map, projectMarker3);
+        infowindow4.close(map, projectMarker4);
+    });
+
+    // Third project
+    var projectMarker3 = new google.maps.Marker({
+        position: {
+            lat: 49.2827,
+            lng: -123.1207
+        },
+        map: map,
+        icon: image,
+        title: 'Project 3'
+    });
+    var contentString3 =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<a href="./burger.html" class="grid-burger__item for-map">' +
+        '<img src="./img/photo-burger3.jpg" class="img-map" alt="...">' +
+        '<span class="resto-name">Restaurant Name</span>' +
+        '<span class="burger-name">Poutine Name</span>' +
+        '<div class="d-flex justify-content-between">' +
+        '<span class="price">$18</span>' +
+        '<span class="distance">1.2km</span>' +
+        '</div>' +
+        '</a>' +
+        '</div>';
+
+    var infowindow3 = new google.maps.InfoWindow({
+        content: contentString3
+    });
+    projectMarker3.addListener('click', function () {
+        infowindow3.open(map, projectMarker3);
+        infowindow2.close(map, projectMarker2);
+        infowindow1.close(map, projectMarker1);
+        infowindow4.close(map, projectMarker4);
+    });
+
+    // Fourth project
+    var projectMarker4 = new google.maps.Marker({
+        position: {
+            lat: 53.5461,
+            lng: -113.4938
+        },
+        map: map,
+        icon: image,
+        title: 'Project 4'
+    });
+    var contentString4 =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<a href="./burger.html" class="grid-burger__item for-map">' +
+        '<img src="./img/photo-burger4.jpg" class="img-map" alt="...">' +
+        '<span class="resto-name">Restaurant Name</span>' +
+        '<span class="burger-name">Poutine Name</span>' +
+        '<div class="d-flex justify-content-between">' +
+        '<span class="price">$18</span>' +
+        '<span class="distance">1.2km</span>' +
+        '</div>' +
+        '</a>' +
+        '</div>';
+
+    var infowindow4 = new google.maps.InfoWindow({
+        content: contentString4
+    });
+    projectMarker4.addListener('click', function () {
+        infowindow4.open(map, projectMarker4);
+        infowindow2.close(map, projectMarker2);
+        infowindow3.close(map, projectMarker3);
+        infowindow1.close(map, projectMarker1);
+    });
+
+}
